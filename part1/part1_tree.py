5240ba42814056c4fe8b7dfbfeb46a78ab0101e1
sannawag <scwager@indiana.edu>
2016-11-17 13:23:55 -0500

    small changes

--- part1/part1_tree.py
+++ part1/part1_tree.py
@@ -4,13 +4,17 @@
 import numpy as np
 import os
 
-directory = "/train/spam"
+directory = "train/spam"
 words = set()
 for filename in os.listdir(directory):
   text_zone = False
-  with open(filename) as file:
+  with open(os.path.join(directory,filename)) as file:
     for line in file:
       if "X-Spam-Level:" in line:
         text_zone = True
+        print ("hello")
       if text_zone:
+        words.add( set(line.split()) )
+    print(words)
+    quit()
         
